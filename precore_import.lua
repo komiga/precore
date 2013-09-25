a2867093ee5f4d04611c1ca024a55d150f66e135
Coranna Howard <me@komiga.com>
2013-09-24 21:28:11 -0400

    Added precore_import helper.

---
+++ precore_import.lua
@@ -0,0 +1,17 @@
+
+--[[
+	This imports precore by an environment variable. It can be used in
+	a project to avoid submoduling.
+--]]
+
+local env_name = "PRECORE_ROOT"
+local root_path = os.getenv(env_name)
+
+if nil == root_path or 0 == #root_path then
+	error(
+		"Environment variable '" .. env_name .. "' is not " ..
+		"defined or is blank; assign it to precore's root directory"
+	)
+end
+
+dofile(path.join(os.getenv(env_name), "precore.lua"))
